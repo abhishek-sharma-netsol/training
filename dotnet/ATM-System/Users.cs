787e1af41b00c1858dd6076d5f5bbd63f8665f12
Abhishek Sharma <abhishek.sharma@netsolutions@gmail.com>
2022-01-16 23:35:25 +0530

    added ATM-System Project

---
+++ dotnet/ATM-System/Users.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ATM {
+    public class PersonDetail{
+        public int Code{get;set;}
+        public int Balance{get;set;}
+        public string? Name{get;set;}=default;
+
+    }
+}
