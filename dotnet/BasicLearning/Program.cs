b54d200209aa400e19e54eaf96f13b6ecd7994aa
Abhishek Sharma <abhishek.sharma@netsolutions@gmail.com>
2022-01-17 10:28:04 +0530

    updated

--- dotnet/BasicLearning/Program.cs
+++ dotnet/BasicLearning/Program.cs
@@ -5,24 +5,29 @@ namespace BasicLearning
 
     class Program
     {
-        static void Main(string[] args)
-        {
-            string[] student = new string[3]{"student1", "student2", "student3"};
-            foreach(string s in student){
-                Console.WriteLine(s);
-            }
+      public unsafe void test(){
+        int var = 20;
+        int *p = &var;
+      }
+      
+      static void Main(string[] args)
+       {
+         string[] student = new string[3]{"student1", "student2", "student3"};
+         foreach(string s in student){
+             Console.WriteLine(s);
+         }
 
-            // Clears element in an array
-            Array.Clear(student, 0, 2);
-            foreach(string s in student){
-                Console.WriteLine(s);
-            }
+         // Clears element in an array
+         Array.Clear(student, 0, 2);
+         foreach(string s in student){
+             Console.WriteLine(s);
+         }
 
-            // Length of an array
-            Console.WriteLine(student.Length);
+         // Length of an array
+         Console.WriteLine(student.Length);
 
-            // get Index of first occurrence of specified object
-            Console.WriteLine(Array.IndexOf(student,"student3"));
-        }
+         // get Index of first occurrence of specified object
+         Console.WriteLine(Array.IndexOf(student,"student3"));
+       }
     }
 }
