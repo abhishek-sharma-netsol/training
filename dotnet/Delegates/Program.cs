785b4fda5d33b07e00c0eb1ad24de5c6b30a68c4
Abhishek Sharma <abhishek.sharma@netsolutions@gmail.com>
2022-01-19 15:40:41 +0530

    updated

---
+++ dotnet/Delegates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+delegate int NumberChanger(int n);
+namespace Delegates
+{
+  class PrintString {
+    static FileStream fs;
+    static StreamWriter sw;
+
+    public delegate void printString(string s);
+    public static void WriteToScreen(string str){
+      Console.WriteLine("The String is: {0}", str);
+    }
+
+    public static void WritToFile(string s){
+      fs = new FileStream("message.txt",FileMode.Append, FileAccess.Write);
+    }
+  }
+  class TestDelegate {
+    static int num = 10;
+    public static int AddNum(int p){
+      num += p;
+      return num;
+    }
+    public static int MultNum(int q){
+      num *= q;
+      return num;
+    }
+    public static getNum(){
+      return num;
+    }
+  }
+
+  class Program{
+    static void Main(string[] args){
+      NumberChanger nc1 = new NumberChanger(AddNum);
+      NumberChanger nc2 = new NumberChanger(MultNum);
+      
+      nc1(25);
+      Console.WriteLine("Value of Num: {0}", getNum());
+      nc2(5);
+      Console.WriteLine("Value of Num: {0}", getNum());
+      Console.ReadLine();
+    }
+  }
+}
