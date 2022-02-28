d12c3bf1e451cd754b1e82037380ec8aef644843
Abhishek Sharma <abhisheksharma521020@gmail.com>
2022-02-28 19:10:44 +0530

    updated

--- dotnet/Web API/NS.EMS.API/Program.cs
+++ dotnet/Web API/NS.EMS.API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Microsoft.OpenApi.Models;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 
@@ -9,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
    options=>
    {
@@ -26,13 +27,45 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
    }
 );
 
+//builder.Services.AddSwaggerGen( swagger => 
+//    swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "EMS", Version="v1"}) 
+//);
+builder.Services.AddSwaggerGen(swagger =>
+            {
+                swagger.IgnoreObsoleteActions();
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "EMS", Version = "v1" });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Add Bearer Token in Header"
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                    new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference  {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id =  "Bearer"
+                              }
+                          },
+                    new string[] { }
+                    }
+                  }
+                      );
+            });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(swagger => swagger.SwaggerEndpoint("/swagger/v1/swagger.json","NS.EMS.API v1"));
 }
 
 app.UseHttpsRedirection();
